f4082c8a8ecf8b966c0ea237851b3c8170b0fb14
DaniloRCBGG <danilorios67@gmail.com>
2025-08-01 10:48:47 -0300

    fix: Variaveis de presquisa corrigidas

--- pesquisa_do.py
+++ pesquisa_do.py
@@ -15,7 +15,7 @@ from prefect.variables import Variable
 load_dotenv()
 sender_email_credentials = Secret.load("nao-responda-email-credentials").get()
 recipient_email_credentials = Secret.load("sigeo-email-credentials").get()
-do_keys_to_search = Variable.get("do_aplication_search_keys")["VALUES"]
+do_keys_to_search = Variable.get("search_keys")["VALUES"]
 
 # Mapeamento dos meses para abreviações em português
 MESES_PT = {
